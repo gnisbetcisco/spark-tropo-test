97b61979a1f77f27fbd7be847debe66c9085b906
gnisbetcisco <gnisbet@cisco.com>
2016-07-27 12:45:48 +0100

    Added /hello

--- george.py
+++ george.py
@@ -4,6 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+    
+    
+@app.route("/hello")
+def hello():
+    return "Hello George!"
 
 if __name__ == "__main__":
     app.run()
