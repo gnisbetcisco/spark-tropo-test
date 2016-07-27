bb627c3a522aa322e8e9546a482ca33e34e46cb3
gnisbetcisco <gnisbet@cisco.com>
2016-07-27 15:42:05 +0100

    Modified george.py

--- george.py
+++ george.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 @app.route("/", methods = ['POST', 'GET'])
 def hello():
 	if(request.method == 'POST'):
-		return "POSTED"
+		return request
 	else:
 		return "GET"
+
+
+
+@app.route("/webhook", methods = ['POST'])
+def webhook():
+	return request
+
 
 if __name__ == "__main__":
     app.run()
