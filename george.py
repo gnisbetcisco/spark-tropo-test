2da142ecebad45d2b6ef076d23d43053ce38456b
gnisbetcisco <gnisbet@cisco.com>
2016-07-27 13:14:52 +0100

    Modified george.py

--- george.py
+++ george.py
@@ -4,14 +4,6 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
-    
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return request.method
-    else:
-        return request.method
 
 if __name__ == "__main__":
     app.run()
