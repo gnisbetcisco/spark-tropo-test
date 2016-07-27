c42f631c619e836b5ca2df3d5038d241c9f2d341
gnisbetcisco <gnisbet@cisco.com>
2016-07-27 15:37:11 +0100

    Modified george.py

--- george.py
+++ george.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def hello():
-    return str(request)
+	if(request.method == 'POST'):
+		return "POSTED"
+	else:
+		return "GET"
 
 if __name__ == "__main__":
     app.run()
