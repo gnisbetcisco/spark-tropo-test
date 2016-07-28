1ee6578e6c48441bdd0afda8f9f29595e33422fe
gnisbetcisco <gnisbet@cisco.com>
2016-07-28 11:51:38 +0100

    Trying parameter things again

--- george.py
+++ george.py
@@ -10,12 +10,14 @@ def hello():
 	else:
 		return "GET"
 
+'''
 @app.route("/init", methods = ['GET'])
 def init():
 	at = request.args.get('auth', '')
 	print(at)
 	return "Bearer " + at
 	#pyCiscoSpark.post_createroom()
+'''
 
 @app.route("/webhook", methods = ['POST'])
 def webhook():
